5807b4db178881975d8d7c368cb434e33605f466
Sapphire Exile <sohzassassinsteam@gmail.com>
2021-07-20 17:41:43 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.WIrelessIPLabel = New System.Windows.Forms.Label()
+        Me.USBSerialLabel = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ConstateLabel = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.AuthStateLabel = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.DebloatBox = New System.Windows.Forms.CheckedListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.ConstateLabel)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.AuthStateLabel)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.USBSerialLabel)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.WIrelessIPLabel)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(364, 13)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 112)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(4, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Wireless IP:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'WIrelessIPLabel
+        '
+        Me.WIrelessIPLabel.Location = New System.Drawing.Point(79, 4)
+        Me.WIrelessIPLabel.Name = "WIrelessIPLabel"
+        Me.WIrelessIPLabel.Size = New System.Drawing.Size(118, 23)
+        Me.WIrelessIPLabel.TabIndex = 1
+        Me.WIrelessIPLabel.Text = "0.0.0.0"
+        Me.WIrelessIPLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'USBSerialLabel
+        '
+        Me.USBSerialLabel.Location = New System.Drawing.Point(79, 27)
+        Me.USBSerialLabel.Name = "USBSerialLabel"
+        Me.USBSerialLabel.Size = New System.Drawing.Size(118, 23)
+        Me.USBSerialLabel.TabIndex = 3
+        Me.USBSerialLabel.Text = "Unknown"
+        Me.USBSerialLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(4, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 23)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "USB Serial: "
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'ConstateLabel
+        '
+        Me.ConstateLabel.Location = New System.Drawing.Point(107, 73)
+        Me.ConstateLabel.Name = "ConstateLabel"
+        Me.ConstateLabel.Size = New System.Drawing.Size(90, 23)
+        Me.ConstateLabel.TabIndex = 7
+        Me.ConstateLabel.Text = "Not connected"
+        Me.ConstateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(4, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(110, 23)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Connection Status"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'AuthStateLabel
+        '
+        Me.AuthStateLabel.Location = New System.Drawing.Point(120, 50)
+        Me.AuthStateLabel.Name = "AuthStateLabel"
+        Me.AuthStateLabel.Size = New System.Drawing.Size(77, 23)
+        Me.AuthStateLabel.TabIndex = 5
+        Me.AuthStateLabel.Text = "Unauthorized"
+        Me.AuthStateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(4, 50)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(110, 23)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Authorization State:"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'DebloatBox
+        '
+        Me.DebloatBox.FormattingEnabled = True
+        Me.DebloatBox.Items.AddRange(New Object() {"Bixby", "Samsung Pass / Samsung Pay", "Facebook", "Samsung Game Launcher", "Gear VR", "DeX", "LED Cover", "Samsung Browser", "Samsung Email", "Print Services", "Kids", "[More coming soon]"})
+        Me.DebloatBox.Location = New System.Drawing.Point(13, 13)
+        Me.DebloatBox.Name = "DebloatBox"
+        Me.DebloatBox.Size = New System.Drawing.Size(176, 184)
+        Me.DebloatBox.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(13, 379)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(551, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Debloat my ass"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(371, 196)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(193, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Reboot my ass"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(576, 414)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DebloatBox)
+        Me.Controls.Add(Me.Panel1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.Name = "Form1"
+        Me.Text = "Sapphire's S21 Debloater"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents USBSerialLabel As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents WIrelessIPLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ConstateLabel As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents AuthStateLabel As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents DebloatBox As CheckedListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,277 @@
+﻿Public Class Form1
+
+    Dim IsWireless = False
+    Dim IsUSB = False
+    Dim IsAuthorized = False
+
+    Dim WirelessIP As String = "000.000.000.000"
+    Dim USBSerial As String = "Unknown"
+
+    Public Shared ReadOnly Bixby() As String = {"com.samsung.android.bixby.wakeup", "com.samsung.android.app.spage", "com.samsung.android.app.routines", "com.samsung.android.bixby.service", "com.samsung.android.visionintelligence", "com.samsung.android.bixby.agent", "com.samsung.android.bixby.agent.dummy", "com.samsung.android.bixbyvision.framework"}
+    Public Shared ReadOnly PassPay() As String = {"com.samsung.android.samsungpassautofill", "com.samsung.android.authfw", "com.samsung.android.samsungpass", "com.samsung.android.spay", "com.samsung.android.spayfw"}
+    Public Shared ReadOnly Facebook() As String = {"com.facebook.katana", "com.facebook.system", "com.facebook.appmanager", "com.facebook.services"}
+    Public Shared ReadOnly GameLauncher() As String = {"com.samsung.android.game.gamehome", "com.enhance.gameservice", "com.samsung.android.game.gametools", "com.samsung.android.game.gos", "com.samsung.android.gametuner.thin"}
+    Public Shared ReadOnly GearVR() As String = {"com.samsung.android.hmt.vrsvc", "com.samsung.android.app.vrsetupwizardstub", "com.samsung.android.hmt.vrshell", "com.google.vr.vrcore"}
+    Public Shared ReadOnly Dex() As String = {"com.sec.android.desktopmode.uiservice", "com.samsung.desktopsystemui", "com.sec.android.app.desktoplauncher"}
+    Public Shared ReadOnly LedCover() As String = {"com.samsung.android.app.ledbackcover", "com.sec.android.cover.ledcover"}
+    Public Shared ReadOnly SamBrowser() As String = {"com.sec.android.app.sbrowser", "com.samsung.android.app.sbrowseredge"}
+    Public Shared ReadOnly SamEmail() As String = {"com.samsung.android.email.provider", "com.wsomacp"}
+    Public Shared ReadOnly Print() As String = {"com.android.bips", "com.google.android.printservice.recommendation", "com.android.printspooler"}
+    Public Shared ReadOnly Kids() As String = {"com.samsung.android.kidsinstaller", "com.samsung.android.app.camera.sticker.facearavatar.preload"}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not (My.Computer.FileSystem.FileExists("adb.exe")) Then
+            IO.File.WriteAllBytes(Application.StartupPath + "\\adb.exe", My.Resources.adb)
+        End If
+        If Not (My.Computer.FileSystem.FileExists("AdbWinApi.dll")) Then
+            IO.File.WriteAllBytes(Application.StartupPath + "\\AdbWinApi.dll", My.Resources.AdbWinApi)
+        End If
+        If Not (My.Computer.FileSystem.FileExists("AdbWinUsbApi.dll")) Then
+            IO.File.WriteAllBytes(Application.StartupPath + "\\AdbWinUsbApi.dll", My.Resources.AdbWinUsbApi)
+        End If
+
+        MsgBox("Because I am fucking lazy, do NOT have multiple devices attached, this includes over tcpip and USB. Disconnecting from every device now." + vbNewLine + vbNewLine + "It is advised to authorize your phone BEFORE starting this program, as again, I am lazy, and haven't really put protections in against an unauthorized device.")
+        Exec("disconnect")
+
+        GetConnectedDevices()
+    End Sub
+
+    Private Function Exec(ByVal Arguments As String)
+        Dim oProcess As New Process()
+        Dim oStartInfo As New ProcessStartInfo("adb.exe", Arguments)
+        oStartInfo.UseShellExecute = False
+        oStartInfo.CreateNoWindow = True
+        oStartInfo.RedirectStandardError = True
+        oStartInfo.RedirectStandardOutput = True
+        oProcess.StartInfo = oStartInfo
+        oProcess.Start()
+
+        Dim sOutput As String
+        Using oStreamReader As System.IO.StreamReader = oProcess.StandardOutput
+            sOutput = oStreamReader.ReadToEnd()
+
+        End Using
+
+        If (sOutput.Length = 0) Then
+            Dim eOutput As String
+            Using oStreamReader As System.IO.StreamReader = oProcess.StandardError
+                eOutput = oStreamReader.ReadToEnd()
+            End Using
+            Console.WriteLine("Error output: " + eOutput)
+            Return eOutput
+        Else
+            Console.WriteLine(sOutput)
+            Return sOutput
+        End If
+
+    End Function
+
+    Private Sub GetConnectedDevices()
+        Dim Ret As String = Exec("devices")
+        Console.WriteLine("Get devices output: " + Ret)
+        Ret = Ret.Remove(0, Ret.IndexOf(ControlChars.CrLf) + 2)
+        If (Ret.Contains(":5555")) Then
+            IsWireless = True
+            WirelessIP = Ret.Substring(0, Ret.IndexOf(":"))
+            WIrelessIPLabel.Text = WirelessIP
+        Else
+            If (Ret.Contains("device")) Then
+                IsUSB = True
+            End If
+        End If
+
+        If (Ret.Contains("device")) Then
+            AuthStateLabel.Text = "Authorized"
+            IsAuthorized = True
+            ConstateLabel.Text = "Connected"
+        End If
+
+        If (Ret.Contains("Unauthorized")) Then
+            AuthStateLabel.Text = "Unauthorized"
+        End If
+    End Sub
+
+    Private Function UninstallApp(ByVal PackageName As String)
+        Dim Ret As String = Exec("shell pm uninstall --user 0 " + PackageName)
+        Console.WriteLine("Uninstall result: " + Ret)
+        If (Ret.Contains("Success")) Then
+            Return True
+        Else
+            If (Ret.Contains("Failure [not installed for 0]")) Then
+                Return True
+            Else
+                Return False
+            End If
+        End If
+    End Function
+
+    Private Sub DoDebloat()
+        For i As Integer = 0 To DebloatBox.Items.Count - 1
+            If DebloatBox.GetItemChecked(i) = True Then
+                If (i = 0) Then
+                    Console.WriteLine("Uninstalling Bixby.")
+                    For BixbyIndex As Integer = 0 To Bixby.Length - 1
+                        Console.WriteLine("Uninstalling package: " + Bixby(BixbyIndex))
+                        Dim RetStatus = UninstallApp(Bixby(BixbyIndex))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + Bixby(BixbyIndex) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 1) Then
+                    Console.WriteLine("Uninstalling Pass / Pay.")
+                    For Index As Integer = 0 To PassPay.Length - 1
+                        Console.WriteLine("Uninstalling package: " + PassPay(Index))
+                        Dim RetStatus = UninstallApp(PassPay(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + PassPay(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 2) Then
+                    Console.WriteLine("Uninstalling Facebook.")
+                    For Index As Integer = 0 To Facebook.Length - 1
+                        Console.WriteLine("Uninstalling package: " + Facebook(Index))
+                        Dim RetStatus = UninstallApp(Facebook(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + Facebook(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 3) Then
+                    Console.WriteLine("Uninstalling Samsung Game Launcher")
+                    For Index As Integer = 0 To GameLauncher.Length - 1
+                        Console.WriteLine("Uninstalling package: " + GameLauncher(Index))
+                        Dim RetStatus = UninstallApp(GameLauncher(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + GameLauncher(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 4) Then
+                    Console.WriteLine("Uninstalling GearVR")
+                    For Index As Integer = 0 To GearVR.Length - 1
+                        Console.WriteLine("Uninstalling package: " + GearVR(Index))
+                        Dim RetStatus = UninstallApp(GearVR(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + GearVR(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 5) Then
+                    Console.WriteLine("Uninstalling DeX")
+                    For Index As Integer = 0 To Dex.Length - 1
+                        Console.WriteLine("Uninstalling package: " + Dex(Index))
+                        Dim RetStatus = UninstallApp(Dex(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + Dex(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 6) Then
+                    Console.WriteLine("Uninstalling Led Cover")
+                    For Index As Integer = 0 To LedCover.Length - 1
+                        Console.WriteLine("Uninstalling package: " + LedCover(Index))
+                        Dim RetStatus = UninstallApp(LedCover(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + LedCover(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 7) Then
+                    Console.WriteLine("Uninstalling Samsung Browser")
+                    For Index As Integer = 0 To SamBrowser.Length - 1
+                        Console.WriteLine("Uninstalling package: " + SamBrowser(Index))
+                        Dim RetStatus = UninstallApp(SamBrowser(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + SamBrowser(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 8) Then
+                    Console.WriteLine("Uninstalling Samsung Email")
+                    For Index As Integer = 0 To SamEmail.Length - 1
+                        Console.WriteLine("Uninstalling package: " + SamEmail(Index))
+                        Dim RetStatus = UninstallApp(SamEmail(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + SamEmail(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 9) Then
+                    Console.WriteLine("Uninstalling Print Services")
+                    For Index As Integer = 0 To Print.Length - 1
+                        Console.WriteLine("Uninstalling package: " + Print(Index))
+                        Dim RetStatus = UninstallApp(Print(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + Print(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+
+                If (i = 10) Then
+                    ' DISABLED
+                    MsgBox("Uninstalling Samsung Kids is currently disabled because the S21 Ultra does not come with it preinstalled. In future version, a check against an installed package list will be done.")
+                    Exit Sub
+                    Console.WriteLine("Uninstalling Kids")
+                    For Index As Integer = 0 To Kids.Length - 1
+                        Console.WriteLine("Uninstalling package: " + Kids(Index))
+                        Dim RetStatus = UninstallApp(Kids(Index))
+                        If (RetStatus = False) Then
+                            MsgBox("Critical failure, failed to uninstall " + Kids(Index) + vbNewLine + "Aborting immediately.")
+                            Exit Sub
+                        End If
+                    Next
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ((IsWireless Or IsUSB) And IsAuthorized) Then
+            Console.WriteLine("Running test.")
+            Dim RetX = Exec("shell whoami")
+            If (RetX.contains("shell")) Then
+                Console.WriteLine("Test passed. Debloating based on choices.")
+            Else
+                MsgBox("Critical failure, failed to get a user from the shell, HARD aborting.")
+                Exit Sub
+            End If
+
+            DoDebloat()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Ret As String = Exec("shell whoami")
+        If (Ret.Contains("shell")) Then
+            Exec("reboot")
+        Else
+            If (Ret.Contains("no devices/emulators found")) Then
+                MsgBox("No device connected.")
+            Else
+                MsgBox("Unknown failure, error description: " + Ret)
+            End If
+        End If
+
+    End Sub
+End Class
